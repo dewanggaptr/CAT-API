8c887e083f7baef660119f19a770b345658c7b30
dewanggaptr <dewanggajr30@gmail.com>
2023-06-23 23:42:59 +0700

    add category model

---
+++ app/Models/Category.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Category extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'name'
+    ];
+
+    protected $hidden = [
+        'created_at',
+        'updated_at'
+    ];
+
+    /**
+     * Get all of the product for the Category
+     *
+     * @return \Illuminate\Database\Eloquent\Relations\HasMany
+     */
+    // public function product(): HasMany
+    // {
+    //     return $this->hasMany(Product::class);
+    // }
+
+    // protected $table = 'categories';
+}
