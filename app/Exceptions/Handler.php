ca43a3f30968967260992a29a2bcaef83261e022
dewanggaptr <dewanggajr30@gmail.com>
2023-06-24 04:11:41 +0700

    adding error handling for 404

--- app/Exceptions/Handler.php
+++ app/Exceptions/Handler.php
@@ -2,6 +2,8 @@
 
 namespace App\Exceptions;
 
+
+use Illuminate\Database\Eloquent\ModelNotFoundException;
 use Illuminate\Foundation\Exceptions\Handler as ExceptionHandler;
 use Throwable;
 
@@ -26,5 +28,15 @@ class Handler extends ExceptionHandler
         $this->reportable(function (Throwable $e) {
             //
         });
+    }
+
+    public function render($request, $exception) {
+        if ($exception instanceof ModelNotFoundException) {
+            return response()->json([
+                'error' => 'record not found'
+            ], 404);
+        }
+
+        return parent::render($request, $exception);
     }
 }
