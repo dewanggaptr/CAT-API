1944898d95804f17136f29929604e01a08f1cb41
dewanggaptr <dewanggajr30@gmail.com>
2023-06-24 14:15:27 +0700

    adding product asset controller

---
+++ app/Http/Controllers/ProductAssetController.php
@@ -0,0 +1,76 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Resources\ApplicationResource;
+use App\Models\ProductAssets;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Storage;
+use Illuminate\Support\Facades\Validator;
+
+class ProductAssetController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        $assets = ProductAssets::all();
+
+        return new ApplicationResource(true, 'list assets data', $assets->load('product'));
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        $validator = Validator::make($request->all(), [
+            'product_id' => 'required|integer',
+            'image' => 'required|image|mimes:png,jpg|max:1024'
+        ]);
+
+        if ($validator->fails()) {
+            return response()->json($validator->errors(), 422);
+        }
+
+        $image = $request->file('image');
+        $image->storeAs('public/product_assets', $image->hashName());
+
+        $asset = ProductAssets::create([
+            'product_id' => $request->product_id,
+            'image' => $image->hashName()
+        ]);
+
+        return new ApplicationResource(true, 'assets created', $asset->load('product'));
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(ProductAssets $asset)
+    {
+        return new ApplicationResource(true, 'assets data found', $asset->load('product'));
+    }
+
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, string $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(ProductAssets $asset)
+    {
+        Storage::delete('public/product_assets/'.$asset->image);
+
+        $asset->delete();
+
+        return new ApplicationResource(true, 'assets deleted', null);
+    }
+}
